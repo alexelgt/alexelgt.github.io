be0322bb0f27d59623533443c663abf0edf4c7ee
alexelgt <alexelgt@gmail.com>
2016-12-01 20:47:05 +0100

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,29 @@
+<?php
+$aviso = "";
+// check form  
+if ($_POST['email'] != "") {
+	// email de destino
+	$email = "contact@alexelgt.com";
+	
+	// asunto del email
+	$subject = "Contact";
+	
+	// Cuerpo del mensaje
+	$mensaje.= "NOMBRE:   ".$_POST['nombre']."\n";
+	$mensaje.= "EMAIL:    ".$_POST['email']."\n";
+	$mensaje.= "IP:       ".$_SERVER['REMOTE_ADDR']."\n\n";
+	$mensaje.= "---------------------------------- \n\n";
+	$mensaje.= $_POST['mensaje']."\n\n";
+	
+	// headers del email
+	$headers = "From: ".$_POST['email']."\r\n";
+	
+	// Enviamos el mensaje
+	if (mail($email, $subject, $mensaje, $headers)) {
+		$aviso = "Message sent successfully.";
+	} else {
+		$aviso = "Error.";
+	}
+}
+header('Location: ' . $_SERVER['HTTP_REFERER']);
+?>
